898cc9d10781cd577bc53ba560a046f150a14d14
masdefry <muhammaddefryan@gmail.com>
2023-01-23 13:09:06 +0700

    Done

--- git-commands.js
+++ git-commands.js
@@ -1,3 +1,4 @@
+// Initiation
 // 1. git init (inside project directory) (warning: first initiate only)
 // 2. git add index.js git-commands.js / git add .
 // 3. git commit -m "Create variable inside index.js"
@@ -5,5 +6,12 @@
 // 5. git remote add origin https://github.com/masdefry/JCWD-2302_SCM.git (warning: first initiate only)
 // 6. git push origin master
 
-// 1. git checkout -b Defryan_NewVariable (Bebas)
-// 2. 
+// Create New Branch
+// 1. git checkout -b namaBranch (Bebas) (Untuk membuat branch baru)
+// 2. git add .
+// 3. git commit -m ""
+// 4. git push origin namaBranch
+
+// Change to other branch
+// 1. git checkout master (Untuk masuk ke suatu branch)
+// 2. git pull origin master (Untuk mendapatkan update dari suatu branch)
